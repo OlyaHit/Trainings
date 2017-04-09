6e1aad3ce0e1afc99fd98461548d031a94415a07
OlyaHit <olyahit@gmail.com>
2017-04-09 23:16:27 +0300

    commit from home

---
+++ src/trainsVideo/Duplicates.java
@@ -0,0 +1,36 @@
+package trainsVideo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+public class Duplicates {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static boolean bruteforce(String[] input) {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input.length; j++) {
+                if (input[i].equals(input[j]) && i != j) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+	public static boolean checkDuplicateUsingSet(String[] input){
+        ArrayList <String> inputList = new ArrayList<String> (Arrays.asList(input));
+        HashSet <String> inputSet = new HashSet <String>(inputList);
+        if(inputSet.size()< inputList.size())
+            return true;
+        return false;
+    }
+
+
+
+}
--- src/trainsVideo/StringAll.java
+++ src/trainsVideo/StringAll.java
@@ -1,20 +1,30 @@
 package trainsVideo;
 
 public class StringAll {
+	
+	public static void main(String[] args){
+		 String str = "abst";
+		 String str2 = "a1b1s1t1";
+		 String habr = "fabrahabr";
+		 addNumbers(str);
+		 isCharPresent(habr);
+		  StringBuilder sb=new StringBuilder("toto,");
+		    System.out.println(sb.deleteCharAt(sb.length()-1));
+		}
 
 	public static void addNumbers(String str){
 		String str2="";
-		StringBuffer stringBuffer = null;
+		StringBuffer sb = null;
 		for(int i=0;i<str.length();i++){
-			stringBuffer = new StringBuffer(str.substring(i, i+1));
-				str2+=stringBuffer.append("1");
+			sb = new StringBuffer(str.substring(i, i+1));
+				str2+=sb.append("1");	
 		}
-		System.out.println(str2);
+		sb= new StringBuffer(str2);
+		System.out.println(sb.deleteCharAt(sb.length()-1));
 	}
 	
 	public static void isCharPresent(String ourChar){
 		 char searchChar = 'h';
-		 int index =0;
 		 boolean result= false;
 		 for (int i = 0; i < ourChar.length(); ++i) {
 		        if (ourChar.charAt(i) == searchChar) {
@@ -28,12 +38,6 @@ public class StringAll {
 
 
 	
-	public static void main(String[] args){
-	 String str = "abst";
-	 String str2 = "a1b1s1t1";
-	 String habr = "fabrahabr";
-	 addNumbers(str);
-	 isCharPresent(habr);
-	}
+	
 
 }
